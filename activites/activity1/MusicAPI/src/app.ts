abe6380e3e29ad015e4f61ab1287d7ec774233d9
Aalaysha Gibson <agibson66@my.gcu.edu>
2026-02-22 22:22:37 -0600

    complete update

--- activites/activity1/MusicAPI/src/app.ts
+++ activites/activity1/MusicAPI/src/app.ts
@@ -3,7 +3,7 @@ import albumsRouter from "./albums/albums.routes";
 import artistsRouter from "./artists/artists.routes";
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 app.use("/albums", albumsRouter);
 app.use("/artists", artistsRouter);
